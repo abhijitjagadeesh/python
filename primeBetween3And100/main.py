a23c1263916ea284258197ebfc33aa423d58b242
Abhijit Jagadeesh <abjagade@synamedia.com>
2022-10-11 23:59:42 +0100

    prime in a range and sorted squared

--- primeBetween3And100/main.py
+++ primeBetween3And100/main.py
@@ -1,14 +1,25 @@
 prime_numbers = []
 
-def is_prime(number):
-    is_prime = True
-    for divisor in range(2, number - 1):
-        if number % divisor == 0:
-            is_prime = False
-    return is_prime
+# def is_prime(number):
+#     is_prime = True
+#     for divisor in range(2, number - 1):
+#         if number % divisor == 0:
+#             is_prime = False
+#     return is_prime
+
+
+# for number in range(3, 101):
+#     if is_prime(number):
+#         prime_numbers.append(number)
+# print(f'{prime_numbers}')
 
 
 for number in range(3, 101):
-    if is_prime(number):
+    prime_number = False
+    for divisor in range(2, number -1):
+        if number % divisor == 0:
+            prime_number = True
+    if not prime_number:
         prime_numbers.append(number)
-print(f'{prime_numbers}')
+
+print(prime_numbers)
---
+++ sortedSquaredArray/main.py
@@ -0,0 +1,6 @@
+array = [1, 2, 3, 4, 5, 6]
+
+def sorted_squared_numbers(input_array):
+    return [number**2 for number in input_array]
+
+print(sorted_squared_numbers(array))
